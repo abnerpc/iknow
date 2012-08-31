2ce0592e9ff356e26bc07185ad2fc9fd591cc584
Abner Campanha <abnerpc@gmail.com>
2012-08-31 00:35:50 -0300

    frame resize and new button again

--- iknow.py
+++ iknow.py
@@ -6,9 +6,9 @@ import wx
 class Iknow(wx.Frame):
     """a simple app to fool around"""
 
-    def __init__(self, *args, **kwargs):
-        
-        super(Iknow, self).__init__(*args, **kwargs) 
+    def __init__(self, parent):
+        style = wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER
+        wx.Frame.__init__(self, parent, size=(600, 160), style=style)
         
         self.dummy_text = "This is only one sentence for test"
         self.changing = False
@@ -19,18 +19,21 @@ class Iknow(wx.Frame):
     def InitUI(self):
         """ build screen components """
 
-        self.panel = wx.Panel(self, size=(600, 400))
+        self.panel = wx.Panel(self)
         
         self.lbl_quote = wx.StaticText(self.panel, label="Question", pos=(20, 30))
         
         self.txt_quote = wx.TextCtrl(self.panel, pos=(20, 50), size=(560, 20))
         self.Bind(wx.EVT_TEXT, self.OnTextChange, self.txt_quote)
         
-        self.btn_answer = wx.Button(self.panel, label="Answer", pos=(250, 100))
+        self.btn_answer = wx.Button(self.panel, label="Answer", pos=(240, 100), size=(120, 50))
         self.Bind(wx.EVT_BUTTON, self.OnAnswerClick, self.btn_answer)
+        self.btn_again = wx.Button(self.panel, label="Again", pos=(240, 100), size=(120, 50))
+        self.btn_again.Hide()
+        self.Bind(wx.EVT_BUTTON, self.OnAgainClick, self.btn_again)
         
-        self.lbl_labelAnswer = wx.StaticText(self.panel, label="Answer:", pos=(20, 200))
-        self.lbl_answer = wx.StaticText(self.panel, pos=(75, 200))        
+        self.lbl_labelAnswer = wx.StaticText(self.panel, label="Answer:", pos=(20, 140))
+        self.lbl_answer = wx.StaticText(self.panel, pos=(75, 140))        
         
         self.SetTitle("Iknow")
         self.Centre()
@@ -59,7 +62,18 @@ class Iknow(wx.Frame):
         #print self.answer
         
     def OnAnswerClick(self, e):
+        if self.txt_quote.IsEmpty():
+            return
+        self.btn_answer.Hide()
+        self.btn_again.Show()
+        self.SetSizeWH(600, 200)
         self.lbl_answer.SetLabel(self.answer)
+
+    def OnAgainClick(self, e):
+        self.btn_again.Hide()
+        self.btn_answer.Show()
+        self.SetSizeWH(600, 160)
+        self.lbl_answer.SetLabel('')
         
 def main():
     
